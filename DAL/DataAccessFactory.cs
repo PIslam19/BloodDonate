c191dfdadf2ec2f1baf32af77b8978f851196881
Optimus_prime <Optimus_prime@DESKTOP-L93F8FM>
2022-12-09 18:25:44 +0600

    Add project files.

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,20 @@
+﻿using DAL.EF;
+using DAL.Interface;
+using DAL.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<BGroup, int, bool> BGroupDataAccess()
+        {
+            return new BGroupRepo();
+
+        }
+    }
+}
---
+++ DAL/Repo/BGroupRepo.cs
@@ -0,0 +1,37 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+   public class BGroupRepo : Interface.IRepo<BGroup, int, bool>
+    {
+        public bool Add( BGroup obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BGroup> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BGroup Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UPDATE(BGroup obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
